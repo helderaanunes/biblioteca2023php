0512cec75fd720f55039a20d1e12abafd797116c
Nikollas Beltrão <beltraonikollas@gmail.com>
2023-03-31 12:00:26 -0300

    UsuarioDao concluido

--- index.php
+++ index.php
@@ -11,16 +11,31 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        require_once $_SERVER['DOCUMENT_ROOT'].'/bibliotecaphp/model/dao/BDPDO.php';
-        require_once $_SERVER['DOCUMENT_ROOT'].'/bibliotecaphp/model/dao/UsuarioDAO.php';
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/bibliotecaphp/model/dao/UsuarioDAO.php';
 
-        $u = new Usuario();
-        $u->setNome("João");
-        $u->setEmail("joao@gmail.com");
-        $u->setSenha("segredo123");
-        $dao = UsuarioDAO::getInstance();
-        $dao->insert($u);
-        echo "Acabou o código...";
+//        $u = new Usuario();
+//        $u->setNome("Vitoria");
+//        $u->setEmail("vitoria@gmail.com");
+//        $u->setSenha("afogados");
+//        UsuarioDAO::getInstance()->insert($u);
+//        echo "Salvou mais um usuario...";
+//        //testando o getById()
+//        $objDoId2= UsuarioDAO::getInstance()->getById(2);
+//        print_r($objDoId2);
+//        //testar o atualizar()
+//        $objDoId2->setLogradouro("Av. Manoel Borba");
+//        $objDoId2->setCidade("Afogados da Ingazeira");
+//        $objDoId2->setSenha("123");
+//        UsuarioDAO::getInstance()->update($objDoId2);
+//        //testar o método de deletar 
+//        UsuarioDAO::getInstance()->delete(6);
+        $arrayDeParametros = array (":login", ":senha");
+        $arrayDeValores = array ("helder", md5("123"));
+        print_r(UsuarioDAO::getInstance()->listWhere(
+                "where login like :login and senha like :senha",
+                $arrayDeParametros,
+                $arrayDeValores
+                ));
         ?>
 
         ?>
--- model/dao/UsuarioDAO.php
+++ model/dao/UsuarioDAO.php
@@ -1,22 +1,24 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'].'/bibliotecaphp/model/dao/BDPDO.php';
-require_once $_SERVER['DOCUMENT_ROOT'].'/bibliotecaphp/model/vo/Usuario.php';
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bibliotecaphp/model/dao/BDPDO.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/bibliotecaphp/model/vo/Usuario.php';
 
 class UsuarioDAO {
-    
+
     public static $instance;
+
     private function __construct() {
+        
     }
+
     public static function getInstance() {
         if (!isset(self::$instance))
             self::$instance = new UsuarioDAO();
- 
+
         return self::$instance;
     }
 
-    
-    
-public function insert(Usuario $usuario){
+    public function insert(Usuario $usuario) {
         try {
             $sql = "INSERT INTO usuario (nome,cpf,logradouro,numero,bairro"
                     . ",cidade,uf,telefone, email,senha) "
@@ -39,13 +41,117 @@ public function insert(Usuario $usuario){
             $p_sql->bindValue(":senha", md5($usuario->getSenha()));
             return $p_sql->execute();
         } catch (Exception $e) {
-            print "Erro ao executar a função de salvar".$e->getMessage();
+            print "Erro ao executar a função de salvar" . $e->getMessage();
         }
     }
 
-public function update ($usuario) {}
-public function delete ($id) { }
-public function getById ($id) { }
-public function listAll(){}
+    public function update($usuario) {
+        try {
+            $sql = "UPDATE usuario SET nome=:nome, cpf =:cpf,logradouro=:logradouro,numero=:numero,"
+                    . "bairro=:bairro, cidade=:cidade,uf =:uf,telefone=:telefone, email=:email, senha=:senha "
+                    . "where id=:id";
+            //perceba que na linha abaixo vai precisar de um import
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            $p_sql->bindValue(":nome", $usuario->getNome());
+            $p_sql->bindValue(":cpf", $usuario->getCpf());
+            $p_sql->bindValue(":logradouro", $usuario->getLogradouro());
+            $p_sql->bindValue(":numero", $usuario->getNumero());
+            $p_sql->bindValue(":bairro", $usuario->getBairro());
+            $p_sql->bindValue(":cidade", $usuario->getCidade());
+            $p_sql->bindValue(":uf", $usuario->getUf());
+            $p_sql->bindValue(":telefone", $usuario->getTelefone());
+            $p_sql->bindValue(":email", $usuario->getEmail());
+            //iremos critografar a senha para md5, asism o usuário terá mais segurança, já que frequentemente usamos a mesma senha           //  para diversas aplicações.
+            $p_sql->bindValue(":senha", md5($usuario->getSenha()));
+            $p_sql->bindValue(":id", $usuario->getId());
+            return $p_sql->execute();
+        } catch (Exception $e) {
+            print "Erro ao executar a função de atualizar" . $e->getMessage();
+        }
+    }
+
+    public function delete($id) {
+        try {
+            $sql = "delete from usuario where id = :id";
+            //perceba que na linha abaixo vai precisar de um import
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            $p_sql->bindValue(":id", $id);
+            return $p_sql->execute();
+        } catch (Exception $e) {
+            print "Erro ao executar a função de deletar --" . $e->getMessage();
+        }
+    }
+
+    public function getById($id) {
+        try {
+            $sql = "SELECT * FROM usuario WHERE id = :id";
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            $p_sql->bindValue(":id", $id);
+            $p_sql->execute();
+            return $this->converterLinhaDaBaseDeDadosParaObjeto($p_sql->fetch(PDO::FETCH_ASSOC));
+        } catch (Exception $e) {
+            print "Ocorreu um erro ao tentar executar esta ação, foi gerado
+ um LOG do mesmo, tente novamente mais tarde.";
+        }
+    }
+
+    private function converterLinhaDaBaseDeDadosParaObjeto($row) {
+        $obj = new Usuario();
+        $obj->setId($row['id']);
+        $obj->setNome($row['nome']);
+        $obj->setCpf($row['cpf']);
+        $obj->setLogradouro($row['logradouro']);
+        $obj->setNumero($row['numero']);
+        $obj->setBairro($row['bairro']);
+        $obj->setCidade($row['cidade']);
+        $obj->setUf($row['uf']);
+        $obj->setTelefone($row['telefone']);
+        $obj->setEmail($row['email']);
+        $obj->setSenha($row['senha']);
+        return $obj;
+    }
+
+    public function listAll() {
+        try {
+            $sql = "SELECT * FROM usuario ";
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+
+            $p_sql->execute();
+
+            $lista = array();
+            $row = $p_sql->fetch(PDO::FETCH_ASSOC);
+            while ($row) {
+                $lista[] = $this->converterLinhaDaBaseDeDadosParaObjeto($row);
+                $row = $p_sql->fetch(PDO::FETCH_ASSOC);
+            }
+            return $lista;
+        } catch (Exception $e) {
+            print "Ocorreu um erro ao tentar executar esta ação, foi gerado
+ um LOG do mesmo, tente novamente mais tarde.";
+        }
+    }
+
+    public function listWhere($restanteDoSQL, $arrayDeParametros, $arrayDeValores) {
+        try {
+            $sql = "SELECT * FROM usuario " . $restanteDoSQL;
+            $p_sql = BDPDO::getInstance()->prepare($sql);
+            for ($i = 0; $i < sizeof($arrayDeParametros); $i++) {
+                $p_sql->bindValue($arrayDeParametros[$i], $arrayDeValores [$i]);
+            }
+            $p_sql->execute();
+            $lista = array();
+            $row = $p_sql->fetch(PDO::FETCH_ASSOC);
+            while ($row) {
+                $lista[] = $this->converterLinhaDaBaseDeDadosParaObjeto($row);
+                $row = $p_sql->fetch(PDO::FETCH_ASSOC);
+            }
+            return $lista;
+        } catch (Exception $e) {
+            print "Ocorreu um erro ao tentar executar esta ação, foi gerado
+ um LOG do mesmo, tente novamente mais tarde.".$e->getMessage();
+        }
+    }
+
 }
+
 ?>
